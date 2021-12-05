0d63f6bb769e65457cb23a14949dabb4ff315146
Alexander Rodionov <a.rodionov@robooption.com>
2021-12-05 18:08:26 +0200

    HW2: string unpack

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,62 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	if str == "" {
+		return "", nil
+	}
+	if unicode.IsDigit(rune(str[0])) {
+		return "", ErrInvalidString
+	}
+	builder := strings.Builder{}
+	temp := str
+	const searching = `0123456789\`
+	for {
+		index := strings.IndexAny(temp, searching)
+		if index == -1 { // ничего не нашли, выходим из цикла
+			builder.WriteString(temp)
+			break
+		}
+		if index > 1 { // собираем все символы до целевого
+			builder.WriteString(temp[:index-1])
+		}
+		lastIdx := len(temp) - 1
+		if unicode.IsDigit(rune(temp[index])) {
+			if index < lastIdx && unicode.IsDigit(rune(temp[index+1])) {
+				return "", ErrInvalidString
+			}
+			char := temp[index-1]
+			number, _ := strconv.Atoi(string(temp[index]))
+			builder.WriteString(strings.Repeat(string(char), number))
+		} else { // slash
+			if index == lastIdx { // слэш в конце, считаем ошибкой
+				return "", ErrInvalidString
+			}
+			if index > 0 { // записываем символ перед найденным
+				builder.WriteRune(rune(temp[index-1]))
+			}
+			nextChar := temp[index+1]
+			if lastIdx >= index+2 && unicode.IsDigit(rune(temp[index+2])) {
+				number, _ := strconv.Atoi(string(temp[index+2]))
+				rep := string(nextChar)
+				if nextChar == 'n' {
+					rep = `\n`
+				}
+				builder.WriteString(strings.Repeat(rep, number))
+				index += 2
+			} else {
+				builder.WriteRune(rune(nextChar))
+				index++
+			}
+		}
+		temp = temp[index+1:]
+	}
+	return builder.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -13,14 +13,17 @@ func TestUnpack(t *testing.T) {
 		expected string
 	}{
 		{input: "a4bc2d5e", expected: "aaaabccddddde"},
+		{input: "a4bc2d5e2", expected: "aaaabccdddddee"},
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `d\n5abc`, expected: `d\n\n\n\n\nabc`},
+		{input: `d\\\n5abc`, expected: `d\\n\n\n\n\nabc`},
 	}
 
 	for _, tc := range tests {
@@ -34,7 +37,12 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{
+		"3abc",
+		"45",
+		"aaa10b",
+		`sdfhskjdfh\`,
+	}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
