bbbdcf59f45aaf3e1070a86927e9a5a4a3ed2da5
Alexander Rodionov <a.rodionov@robooption.com>
2021-12-15 11:28:10 +0200

    HW04: cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -5,12 +5,11 @@ type Key string
 type Cache interface {
 	Set(key Key, value interface{}) bool
 	Get(key Key) (interface{}, bool)
+	Len() int
 	Clear()
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -27,4 +26,46 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	item, exists := c.items[key]
+	if exists {
+		item.Value = newCacheItem(key, value)
+		c.queue.MoveToFront(item)
+	}
+	if !exists {
+		item := c.queue.PushFront(newCacheItem(key, value))
+		c.items[key] = item
+	}
+	if c.queue.Len() > c.capacity {
+		c.queue.Remove(c.queue.Back())
+	}
+	return exists
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	item, exists := c.items[key]
+	if exists {
+		ci := item.Value.(*cacheItem)
+		c.queue.MoveToFront(item)
+		return ci.value, true
+	}
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
+}
+
+func (c *lruCache) Len() int {
+	return c.queue.Len()
+}
+
+func newCacheItem(key Key, value interface{}) *cacheItem {
+	ci := new(cacheItem)
+	ci.key = key
+	ci.value = value
+	return ci
 }
