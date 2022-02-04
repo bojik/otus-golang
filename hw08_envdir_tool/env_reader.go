1519b9c62e8b736ba20fd4611c8883e65bb3861a
Alexander Rodionov <a.rodionov@copybet.com>
2022-02-04 19:00:39 +0200

    HW08: executor

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strings"
+)
+
 type Environment map[string]EnvValue
+
+var (
+	ErrDirDoesNotExist = errors.New("directory does not exist")
+	ErrIsNotDir        = errors.New("given path is not dir")
+)
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
 type EnvValue struct {
@@ -11,6 +25,69 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	stat, err := os.Stat(dir)
+	if os.IsNotExist(err) {
+		return nil, fmt.Errorf("%s: %w", dir, ErrDirDoesNotExist)
+	}
+	if !stat.IsDir() {
+		return nil, fmt.Errorf("%s: %w", dir, ErrIsNotDir)
+	}
+	files, err1 := os.ReadDir(dir)
+	if err1 != nil {
+		return nil, fmt.Errorf("%s: %w", dir, err1)
+	}
+	envs := Environment{}
+	for _, file := range files {
+		if strings.Contains(file.Name(), "=") {
+			continue
+		}
+		fullName := fmt.Sprintf("%s%c%s",
+			strings.TrimRight(dir, string(os.PathSeparator)),
+			os.PathSeparator,
+			file.Name(),
+		)
+		stat, err := os.Stat(fullName)
+		if err != nil {
+			return nil, err
+		}
+		if stat.IsDir() {
+			continue
+		}
+		if stat.Size() == 0 {
+			envs[file.Name()] = EnvValue{
+				NeedRemove: true,
+			}
+			continue
+		}
+		fl, err := readFirstLine(fullName)
+		if err != nil {
+			return nil, err
+		}
+		envs[file.Name()] = EnvValue{
+			Value: clearString(fl),
+		}
+	}
+	return envs, nil
+}
+
+// readFirstLine reads first line only from file given by fileName.
+func readFirstLine(fileName string) (string, error) {
+	fp, err := os.Open(fileName)
+	if err != nil {
+		return "", err
+	}
+	defer fp.Close()
+	con, err := io.ReadAll(fp)
+	if err != nil {
+		return "", err
+	}
+	return strings.Split(string(con), "\n")[0], nil
+}
+
+// clearString prepares string from usage.
+func clearString(s string) string {
+	ret := strings.ReplaceAll(s, "\x00", "\n")
+	re := regexp.MustCompile("[\t ].+$")
+	ret = string(re.ReplaceAll([]byte(ret), []byte{}))
+	return ret
 }
--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,77 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	testDir := "./testdata/env"
+	invFile := testDir + string(os.PathSeparator) + "HZB=HZB"
+	err := os.WriteFile(invFile, []byte("test"), 0x666)
+	require.Nil(t, err)
+	defer os.Remove(invFile)
+	res, err1 := ReadDir(testDir)
+	require.Nil(t, err1)
+	_, ok := res["HZB=HZB"]
+	require.False(t, ok)
+	exp := Environment{
+		"BAR":   EnvValue{Value: "bar", NeedRemove: false},
+		"EMPTY": EnvValue{Value: " ", NeedRemove: false},
+		"FOO":   EnvValue{Value: "   foo\nwith", NeedRemove: false},
+		"HELLO": EnvValue{Value: "\"hello\"", NeedRemove: false},
+		"UNSET": EnvValue{Value: "", NeedRemove: true},
+		//		"UNSET1": EnvValue{Value: "", NeedRemove: true},
+	}
+	require.Equal(t, len(exp), len(res))
+	for k, v := range exp {
+		v1, ok := res[k]
+		if !ok {
+			t.Errorf("key not found: %s", k)
+			t.FailNow()
+		}
+		require.Equal(t, v.Value, v1.Value)
+		require.Equal(t, v.NeedRemove, v1.NeedRemove)
+	}
+}
+
+func TestReadDirError(t *testing.T) {
+	t.Run("err dir does not exist", func(t *testing.T) {
+		res, err := ReadDir("hzhz")
+		require.Nil(t, res)
+		require.ErrorIs(t, err, ErrDirDoesNotExist)
+	})
+	t.Run("err is not dir", func(t *testing.T) {
+		res, err := ReadDir("./go.mod")
+		require.Nil(t, res)
+		require.ErrorIs(t, err, ErrIsNotDir)
+	})
+}
+
+func TestClearText(t *testing.T) {
+	cases := []struct {
+		s string
+		e string
+	}{
+		{"test", "test"},
+		{"test\t ", "test"},
+		{"test\n \t ", "test\n"},
+		{"\t  super test\t \n", "\t  super test\t \n"},
+		{"\t  super test\n\t  ", "\t  super test\n"},
+		{"\t  super test\n\x00\t  ", "\t  super test\n\n"},
+	}
+	for _, tc := range cases {
+		tc := tc
+		t.Run("clear text "+tc.s, func(t *testing.T) {
+			require.Equal(t, tc.e, clearString(tc.s))
+		})
+	}
+}
+
+func TestReadFirstLine(t *testing.T) {
+	fl, err := readFirstLine("./testdata/env/FOO")
+	require.Nil(t, err)
+	require.Equal(t, "   foo\nwith", clearString(fl))
 }
